ae632cef2c5091683fee502a2206f402c8479465
Amanda <amreinhard@icloud.com>
2018-04-26 16:34:46 -0700

    iOS app functionality completed

---
+++ Project37/CardViewController.swift
@@ -0,0 +1,124 @@
+//
+//  CardViewController.swift
+//  Project37
+//
+//  Created by Amanda Reinhard on 4/26/18.
+//  Copyright © 2018 Amanda Reinhard. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+import AVFoundation
+
+class CardViewController: UIViewController {
+    
+    weak var delegate: ViewController!
+    
+    var front: UIImageView!
+    var back: UIImageView!
+    var music: AVAudioPlayer!
+    
+    var isCorrect = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.bounds = CGRect(x: 0, y: 0, width: 100, height: 140)
+        front = UIImageView(image: UIImage(named: "cardBack"))
+        back = UIImageView(image: UIImage(named: "cardBack"))
+        
+        view.addSubview(front)
+        view.addSubview(back)
+        
+        playMusic()
+        
+        front.isHidden = true
+        back.alpha = 0
+        
+        UIView.animate(withDuration: 0.2) {
+            self.back.alpha = 1
+        }
+        
+        let tap = UIGestureRecognizer(target: self, action: #selector(cardTapped))
+        back.isUserInteractionEnabled = true
+        back.addGestureRecognizer(tap)
+        
+        perform(#selector(wiggle), with: nil, afterDelay: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func cardTapped() {
+        delegate.cardTapped(self)
+    }
+    
+    @objc func wasntTapped() {
+        UIView.animate(withDuration: 0.7) {
+            self.view.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+            self.view.alpha = 0
+        }
+    }
+    
+    func wasTapped() {
+        UIView.transition(with: view, duration: 0.7, options: [.transitionFlipFromRight], animations: { [unowned self] in
+            self.back.isHidden = true
+            self.front.isHidden = false
+        })
+    }
+    
+    @objc func wiggle() {
+        if GKRandomSource.sharedRandom().nextInt(upperBound: 4) == 1 {
+            UIView.animate(withDuration: 0.2, delay: 0, options: .allowUserInteraction, animations: {
+                self.back.transform = CGAffineTransform(scaleX: 1.01, y: 1.01)
+            }) { _ in
+                self.back.transform = CGAffineTransform.identity
+            }
+            
+            perform(#selector(wiggle), with: nil, afterDelay: 8)
+        } else {
+            perform(#selector(wiggle), with: nil, afterDelay: 2)
+        }
+    }
+    
+    func playMusic() {
+        if let musicURL = Bundle.main.url(forResource: "PhantomFromSpace", withExtension: "mp3") {
+            if let audioPlayer = try? AVAudioPlayer(contentsOf: musicURL) {
+                music = audioPlayer
+                music.numberOfLoops = -1
+                music.play()
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: cardContainer)
+        
+        for card in allCards {
+            if card.view.frame.contains(location) {
+                if view.traitCollection.forceTouchCapability == .available {
+                    if touch.force == touch.maximumPossibleForce {
+                        card.front.image = UIImage(named: "cardStar")
+                        card.isCorrect = true
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project37/ViewController.swift
+++ Project37/ViewController.swift
@@ -7,19 +7,120 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
+    
+    var allCards = [CardViewController]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        createParticles()
+        loadCards()
+        
+        view.backgroundColor = UIColor.red
+        
+        UIView.animate(withDuration: 20, delay: 0, options: [.allowUserInteraction, .autoreverse, .repeat], animations: {
+            self.view.backgroundColor = UIColor.blue
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func loadCards() {
+        for card in allCards {
+            card.view.removeFromSuperview()
+            card.removeFromParentViewController()
+        }
+        
+        view.isUserInteractionEnabled = true
+        
+        allCards.removeAll(keepingCapacity: true)
+        let positions = [
+            CGPoint(x: 75, y: 85),
+            CGPoint(x: 185, y: 85),
+            CGPoint(x: 295, y: 85),
+            CGPoint(x: 405, y: 85),
+            CGPoint(x: 75, y: 235),
+            CGPoint(x: 185, y: 235),
+            CGPoint(x: 295, y: 235),
+            CGPoint(x: 405, y: 235)
+        ]
+        
+        let circle = UIImage(named: "cardCircle")!
+        let cross = UIImage(named: "cardCross")!
+        let lines = UIImage(named: "cardLines")!
+        let square = UIImage(named: "cardSquare")!
+        let star = UIImage(named: "cardStar")!
+        
+        var images = [circle, circle, cross, cross, lines, lines, square, star]
+        images = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: images) as! [UIImage]
+        
+        for (index, position) in positions.enumerated() {
+            let card = CardViewController()
+            card.delegate = self
+            
+            addChildViewController(card)
+            cardContainer.addSubview(card.view)
+            card.didMove(toParentViewController: self)
+            
+            card.view.center = position
+            card.front.image = images[index]
+            
+            if card.front.image == star {
+                card.isCorrect = true
+            }
+            
+            allCards.append(card)
+        }
+    }
+    
+    func createParticles() {
+        let particleEmitter = CAEmitterLayer()
+        
+        particleEmitter.emitterPosition = CGPoint(x: view.frame.width / 2.0, y: -50)
+        particleEmitter.emitterShape = kCAEmitterLayerLine
+        particleEmitter.emitterSize = CGSize(width: view.frame.width, height: 1)
+        particleEmitter.renderMode = kCAEmitterLayerAdditive
+        
+        let cell = CAEmitterCell()
+        cell.birthRate = 2
+        cell.lifetime = 5.0
+        cell.velocity = 100
+        cell.velocityRange = 50
+        cell.emissionLongitude = .pi
+        cell.spinRange = 5
+        cell.scale = 0.5
+        cell.scaleRange = 0.25
+        cell.color = UIColor(white: 1, alpha: 0.1).cgColor
+        cell.alphaSpeed = -0.025
+        cell.contents = UIImage(named: "particle")?.cgImage
+        particleEmitter.emitterCells = [cell]
+        
+        gradientView.layer.addSublayer(particleEmitter)
+    }
+    
+    func cardTapped(_ tapped: CardViewController) {
+        guard view.isUserInteractionEnabled == true else { return }
+        view.isUserInteractionEnabled = false
+        
+        for card in allCards {
+            if card == tapped {
+                card.wasTapped()
+                card.perform(#selector(card.wasntTapped), with: nil, afterDelay: 1)
+            } else {
+                card.wasntTapped()
+            }
+        }
+        perform(#selector(loadCards), with: nil, afterDelay: 2)
+    }
 
 
+    @IBOutlet var gradientView: GradientView!
+    @IBOutlet var cardContainer: UIView!
 }
 
